9704df5e2c36d63701f7c0a21a4e77e011f0e94f
Vishnu <chvvreddy4@gmail.com>
2026-09-17 20:28:04 +0530

    Update quantum subtractor with user input

--- quantum_subtractor.py
+++ quantum_subtractor.py
@@ -1,16 +1,50 @@
 from qiskit import QuantumCircuit
 
-# Create 2 qubits and 2 classical bits
-qc = QuantumCircuit(2, 2)
+# Get inputs from user
+A = int(input("Enter A (0 or 1): "))
+B = int(input("Enter B (0 or 1): "))
 
-# Inputs: A = 1, B = 0
-qc.x(0)
+# Validate input
+if A not in [0, 1] or B not in [0, 1]:
+    print("Please enter only 0 or 1.")
+else:
+    # 3 qubits:
+    # q0 = A
+    # q1 = B
+    # q2 = Borrow
+    # 2 classical bits:
+    # c0 = Difference
+    # c1 = Borrow
 
-# Difference = A XOR B
-qc.cx(1, 0)
+    qc = QuantumCircuit(3, 2)
 
-# Measure
-qc.measure(0, 0)
-qc.measure(1, 1)
+    # Set input A
+    if A == 1:
+        qc.x(0)
 
-print(qc)
+    # Set input B
+    if B == 1:
+        qc.x(1)
+
+    # -------------------------
+    # Calculate Borrow
+    # Borrow = NOT(A) AND B
+    # -------------------------
+
+    qc.x(0)
+    qc.ccx(0, 1, 2)
+    qc.x(0)
+
+    # -------------------------
+    # Calculate Difference
+    # Difference = A XOR B
+    # -------------------------
+
+    qc.cx(1, 0)
+
+    # Measure
+    qc.measure(0, 0)   # Difference
+    qc.measure(2, 1)   # Borrow
+
+    print("\nQuantum Half Subtractor:")
+    print(qc)
